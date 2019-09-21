b70cce193932423c85cb848a1baf3824d45edb5e
maruhachi <konchan1129@gmail.com>
2019-09-21 11:47:27 +0900

    compileが通るようにした。 お試し用のpublicメソッドを生やした

---
+++ src/main/java/work/yk0/SugokuBenriMethod.java
@@ -0,0 +1,23 @@
+package work.yk0;
+
+/**
+ * Hello world!
+ *
+ */
+public class SugokuBenriMethod
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    /**
+     * 近くて便利な引数を受け取り、それがいかに便利であるか返します.
+     * @param param 便利な何か
+     * @return いかに便利か
+     */
+    public String benriMethod(String param){
+        return "近くて便利、" + param + "！！";
+    }
+
+}
